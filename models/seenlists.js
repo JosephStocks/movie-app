90255585085b8078a258ec76e64133abc61608f3
Joseph Stocks <jpstocks63@gmail.com>
2021-02-01 11:29:03 -0600

    fixed user lists sql table associations

--- models/seenlists.js
+++ models/seenlists.js
@@ -1,26 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class seenlists extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.seenlists.belongsTo(models.users, {foreignKey: 'userid'})
-      models.seenlists.hasMany(models.movies, {foreignKey: 'movieid'})
+    class seenlists extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            models.seenlists.belongsTo(models.users, { foreignKey: "userid" });
+            models.seenlists.belongsTo(models.movies, {
+                foreignKey: "movieid",
+            });
+        }
     }
-  };
-  seenlists.init({
-    userid: DataTypes.INTEGER,
-    movieid: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'seenlists',
-  });
-  return seenlists;
+    seenlists.init(
+        {
+            userid: DataTypes.INTEGER,
+            movieid: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: "seenlists",
+        }
+    );
+    return seenlists;
 };
